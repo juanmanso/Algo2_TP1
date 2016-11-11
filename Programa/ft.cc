435b9bbe2a97896d907780a7590315bff2c27ff0
Juan Manso <juanmanso@gmail.com>
2016-11-11 01:24:31 -0300

    Commit inicial

---
+++ Programa/ft.cc
@@ -0,0 +1,15 @@
+
+// Aqui se implementan las funciones de calculo de fft y dft
+
+#include <string>
+
+
+using namespace std;
+
+void fft::calc(){
+	// calculo
+}
+
+void dft::calc(){
+	// calculo
+}
---
+++ Programa/ft.h
@@ -0,0 +1,35 @@
+#ifndef _FT_H_INCLUDED_
+#define _FT_H_INCLUDED_
+
+
+// Aquí se debe definir la clase ft, la cual será abstracta. Ésta se encarga
+// de indicar el método de cálculo a utilizar (i.e. DFT, FFT).
+//
+// Asímismo, se deben definir las clases "hijas" como DFT y FFT.
+
+
+class ft 
+{
+	public:
+		virtual void calc()=0
+
+}
+
+
+
+class dft : public ft
+{
+	public:
+		void calc()
+}
+
+
+
+class fft : public ft
+{
+	public:
+		void calc()
+
+}
+
+#endif
